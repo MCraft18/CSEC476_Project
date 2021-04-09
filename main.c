e34ba8d7825cbdb4c4bc2ee77c9635484236a5c7
mdc2913 <mdc2913@rit.edu>
2021-04-09 19:01:03 -0400

    server and client files made

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "server.c"
 
 int main() {
     printf("Hello, Worlda!\n");
---
+++ server.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int serv()
+{
+
+    printf("Initialised.");
+    return 0;
+}
