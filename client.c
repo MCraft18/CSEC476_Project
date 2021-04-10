3e8f3a8fce9068f43ba236fdfce5f909f74ec955
mdc2913 <mdc2913@rit.edu>
2021-04-10 16:54:24 -0400

    Added socket to client code

--- client.c
+++ client.c
@@ -4,14 +4,34 @@
 
 int client(){
     WSADATA wsa;
+    SOCKET s;
+    struct sockaddr_in server;
+    char *data;
 
     printf("\nInitialising Winsock...");
-    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+    if (WSAStartup(MAKEWORD(2,2),&wsa) == 0)
     {
-        printf("Failed. Error Code : %d",WSAGetLastError());
-        return 1;
+        printf("Initialised.\n");
     }
 
-    printf("Initialised.");
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    printf("Socket Created\n");
+
+    server.sin_addr.s_addr = inet_addr("74.125.235.20");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(80);
+
+    if(connect(s,(struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        printf("Connect error\n");
+    }
+
+    data = "GET / HTTP/1.1\r\n\r\n";
+    if(send(s, data, strlen(data), 0) < 0)
+    {
+       puts("Send failed");
+    }
+
+
     return 0;
 }
--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "server.c"
 #include "client.c"
 int main() {
-    printf("Hello, Worlda!\n");
+    printf("Hello, World!\n");
     serv();
     client();
     return 0;
